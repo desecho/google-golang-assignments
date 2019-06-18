a8a4a5948b30f5399e72fd95e442b95054263c44
Anton Samarchyan <anton.samarchyan@ubisoft.com>
2019-06-18 08:45:28 -0400

    123

--- 3-4/1.go
+++ 3-4/1.go
@@ -8,63 +8,118 @@ import (
 	"time"
 )
 
-const timesEat int = 3
-const numberPhilos int = 5
-const concurrentEaters int = 2
+const (
+	eatingTime       = 300
+	timesEat         = 3
+	numberPhilos     = 5
+	concurrentEaters = 2
+)
 
+var maxMeals int
 var currentEaters []int
 
+// ChopS Chopstick
 type ChopS struct{ sync.Mutex }
 
+var currentEatersLock sync.Mutex
+
+// Philo Philosopher
 type Philo struct {
 	idNumber        int
 	leftCS, rightCS *ChopS
 }
 
-func host(c chan) {
-	for i := 0; i < numberPhilos; i++ {
-		
+func contains(s []int, e int) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
 	}
+	return false
 }
 
 func isNeighbor(a, b int) bool {
 	return math.Abs(float64(a-b)) == 1 || (b == numberPhilos && a == 0) || (a == numberPhilos && b == 0)
 }
 
-func getPermissionFromHost(id int) bool {
-
+func isAllowedToEat(currentEaters []int, id int) bool {
+	if len(currentEaters) < concurrentEaters {
+		if contains(currentEaters, id) {
+			return false
+		}
+		for _, eaterID := range currentEaters {
+			if isNeighbor(eaterID, id) {
+				return false
+			}
+		}
+		return true
+	}
+	return false
 }
 
-func (p Philo) eat(wg *sync.WaitGroup, c chan) {
-	if getPermissionFromHost(p.idNumber) {
-		x := rand.Intn(2)
-		if x == 1 {
-			p.leftCS.Lock()
-			// fmt.Printf("Left chopstick locked for %d\n", p.idNumber)
-			p.rightCS.Lock()
-			// fmt.Printf("Right chopstick locked for %d\n", p.idNumber)
-		} else {
-			p.rightCS.Lock()
-			// fmt.Printf("Right chopstick locked for %d\n", p.idNumber)
-			p.leftCS.Lock()
-			// fmt.Printf("Left chopstick locked for %d\n", p.idNumber)
+func host(wg *sync.WaitGroup, currentEaters *[]int, requestChan chan int, responseChan chan bool) {
+	var meals int
+	for {
+		if meals == maxMeals {
+			wg.Done()
 		}
-		idNumber := p.idNumber + 1
-		fmt.Printf("starting to eat %d\n", idNumber)
-		fmt.Printf("finishing eating %d\n", idNumber)
+		id := <-requestChan
+		answer := isAllowedToEat(*currentEaters, id)
+		if answer {
+			currentEatersLock.Lock()
+			*currentEaters = append(*currentEaters, id)
+			currentEatersLock.Unlock()
+			meals++
+		}
+		responseChan <- answer
+	}
+}
 
-		p.rightCS.Unlock()
-		// fmt.Printf("Right chopstick unlocked for %d\n", p.idNumber)
-		p.leftCS.Unlock()
-		// fmt.Printf("Left chopstick unlocked for %d\n", p.idNumber)
-		wg.Done()
+func removeByValue(slice []int, value int) []int {
+	output := []int{}
+	for _, v := range slice {
+		if v != value {
+			output = append(output, v)
+		}
+	}
+	return output
+}
+
+func (p Philo) eat(wg *sync.WaitGroup, currentEaters *[]int, requestChan chan int, responseChan chan bool) {
+	for {
+		requestChan <- p.idNumber
+		permitted := <-responseChan
+		if permitted {
+			x := rand.Intn(2)
+			if x == 1 {
+				p.leftCS.Lock()
+				p.rightCS.Lock()
+			} else {
+				p.rightCS.Lock()
+				p.leftCS.Lock()
+			}
+			idNumber := p.idNumber + 1
+			fmt.Printf("starting to eat %d\n", idNumber)
+			time.Sleep(eatingTime * time.Millisecond)
+			fmt.Printf("finishing eating %d\n", idNumber)
+
+			p.rightCS.Unlock()
+			p.leftCS.Unlock()
+			currentEatersLock.Lock()
+			*currentEaters = removeByValue(*currentEaters, p.idNumber)
+			currentEatersLock.Unlock()
+			wg.Done()
+			return
+		}
 	}
 }
 
 func main() {
 	var wg sync.WaitGroup
 	rand.Seed(time.Now().UnixNano())
-	c := make(chan int)
+	maxMeals = numberPhilos * timesEat
+	requestChan := make(chan int)
+	responseChan := make(chan bool)
 	currentEaters = make([]int, 0)
 	CSticks := make([]*ChopS, numberPhilos)
 	for i := 0; i < numberPhilos; i++ {
@@ -74,12 +129,12 @@ func main() {
 	for i := 0; i < numberPhilos; i++ {
 		philos[i] = &Philo{i, CSticks[i], CSticks[(i+1)%numberPhilos]}
 	}
-	wg.Add(numberPhilos*timesEat + 1)
+	wg.Add(maxMeals + 1)
 	for i := 0; i < numberPhilos; i++ {
 		for j := 0; j < timesEat; j++ {
-			go philos[i].eat(&wg, c)
+			go philos[i].eat(&wg, &currentEaters, requestChan, responseChan)
 		}
 	}
-	go host(c)
+	go host(&wg, &currentEaters, requestChan, responseChan)
 	wg.Wait()
 }
